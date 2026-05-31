1f8785f5f27db026b1a42358dfda49278fe0e31d
Mohammad Akramul Ahmed Wahid <akramisgood.1@gmail.com>
2026-06-01 04:25:52 +0600

    fixed indentation error in app.py

--- render-demo/app.py
+++ render-demo/app.py
@@ -59,9 +59,7 @@ def predict():
         "index.html",
         prediction_text=result
     )
-
-
- if __name__ == '__main__':
-        import os
+if __name__ == '__main__':
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
